a83f8e69df48bcfcdc7d4fb51b9b0dfa09e2fe53
Michael McManus <melancholic_maclean@yahoo.com>
2023-01-31 00:33:40 -0500

    add home

---
+++ pages/home.py
@@ -0,0 +1,20 @@
+from dash import html, register_page  #, callback # If you need callbacks, import it here.
+
+register_page(
+    __name__,
+    name='Home',
+    top_nav=True,
+    path='/'
+)
+
+
+
+def layout():
+    layout = html.Div([
+        html.H1(
+            [
+                "Home Page"
+            ]
+        )
+    ])
+    return layout
