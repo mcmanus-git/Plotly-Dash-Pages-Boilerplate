a5c102d6026554b6ce4c45aee4dd1b8373f76b5d
Michael McManus <melancholic_maclean@yahoo.com>
2023-01-31 00:22:48 -0500

    add app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+from navbar import create_navbar
+
+# Toggle the themes at [dbc.themes.LUX]
+# The full list of available themes is:
+# BOOTSTRAP, CERULEAN, COSMO, CYBORG, DARKLY, FLATLY, JOURNAL, LITERA, LUMEN, LUX, MATERIA, MINTY, PULSE, SANDSTONE,
+# SIMPLEX, SKETCHY, SLATE, SOLAR, SPACELAB, SUPERHERO, UNITED, YETI, ZEPHYR.
+# https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/
+
+NAVBAR = create_navbar()
+# To use Font Awesome Icons
+FA512 = "https://use.fontawesome.com/releases/v6.2.1/css/all.css"
+APP_TITLE = "First Dash App"
+
+app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[
+        dbc.themes.LUX,  # Dash Themes CSS
+        FA512,  # Font Awesome Icons CSS
+    ],
+    title=APP_TITLE,
+    use_pages=True,
+)
+
+# To use if you're planning on using Google Analytics
+app.index_string = f'''
+<!DOCTYPE html>
+<html>
+    <head>
+        {{%metas%}}
+        <title>{APP_TITLE}</title>
+        {{%favicon%}}
+        {{%css%}}
+    </head>
+    <body>
+        {{%app_entry%}}
+        <footer>
+            {{%config%}}
+            {{%scripts%}}
+            {{%renderer%}}
+        </footer>
+        
+    </body>
+</html>
+'''
+
+app.layout = dcc.Loading(  # <- Wrap App with Loading Component
+    id='loading_page_content',
+    children=[
+        html.Div(
+            [
+                NAVBAR,
+                dash.page_container
+            ]
+        )
+    ],
+    color='primary',  # <- Color of the loading spinner
+    fullscreen=True  # <- Loading Spinner should take up full screen
+)
+
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
