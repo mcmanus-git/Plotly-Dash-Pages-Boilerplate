138a837a07e6145d591fb5fc63dd5a7b0edd305c
Michael McManus <melancholic_maclean@yahoo.com>
2023-01-31 00:36:23 -0500

    add page 2

---
+++ pages/page2.py
@@ -0,0 +1,20 @@
+from dash import html, register_page  #, callback # If you need callbacks, import it here.
+
+register_page(
+    __name__,
+    name='Page 2',
+    top_nav=True,
+    path='/page2'
+)
+
+
+
+def layout():
+    layout = html.Div([
+        html.H1(
+            [
+                "Page 2"
+            ]
+        )
+    ])
+    return layout
